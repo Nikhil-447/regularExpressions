db7485b4661a695d89689d7268de1ef283e925e4
Nikky-kallaje <nkallaje447@gmail.com>
2020-07-07 19:26:21 +0530

    modifued code to verify user last

--- userReg/userRegistartion.sh
+++ userReg/userRegistartion.sh
@@ -1,14 +1,32 @@
 #!/bin/bash -x
 
-shopt -s extglob
-pat="^[A-Z]{1}[a-zA-Z]{2,}$"
-echo "Enter The Last Name"
-read name
 
-if [[ $pat =~ $name ]]
-then
-   echo "Success"
-else
-   echo "Invalid Entry, Please Try Again"
-fi
 
+while [ !=0 ]
+do
+
+	shopt -s extglob
+	pat="^[A-Z]{1}[a-zA-Z]{2,}$"
+ 	echo "Enter The First Name"
+	read fname
+
+	if [[ $pat =~ $fname ]]
+	then
+      	   echo "Success"
+	else
+   	   echo "Invalid Entry, Please Try Again"
+	   continue;
+        fi
+
+	echo "Enter The Last Name"
+        read lname
+
+        if [[ $pat =~ $lname ]]
+        then
+           echo "Success"
+        else
+           echo "Invalid Entry, Please Try Again"
+           continue;
+        fi
+
+done
